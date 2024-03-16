21a2ad358c48993e981224ebad61e7b4c38cfdf7
achimvp <prittwitza@yahoo.com>
2024-03-16 15:49:06 +0100

    Started tutorial 11 normalizing flows

---
+++ UAV_tutorials/tutorial_11_normalizing_flows.py
@@ -0,0 +1,337 @@
+## Standard libraries
+import os
+import math
+import time
+import numpy as np
+
+## Imports for plotting
+import matplotlib.pyplot as plt
+from matplotlib.colors import to_rgb
+import matplotlib
+matplotlib.rcParams['lines.linewidth'] = 2.0
+import seaborn as sns
+sns.reset_orig()
+
+## Progress bar
+from tqdm.notebook import tqdm
+
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+# Torchvision
+import torchvision
+from torchvision.datasets import MNIST
+from torchvision import transforms
+# PyTorch Lightning
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+# Path to the folder where the datasets are/should be downloaded (e.g. MNIST)
+DATASET_PATH = "../data"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "../saved_models/tutorial11"
+
+# Setting the seed
+pl.seed_everything(42)
+
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+# Fetching the device that will be used throughout this notebook
+device = torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0")
+print("Using device", device)
+
+import urllib.request
+from urllib.error import HTTPError
+# Github URL where saved models are stored for this tutorial
+base_url = "https://raw.githubusercontent.com/phlippe/saved_models/main/tutorial11/"
+# Files to download
+pretrained_files = ["MNISTFlow_simple.ckpt", "MNISTFlow_vardeq.ckpt", "MNISTFlow_multiscale.ckpt"]
+# Create checkpoint path if it doesn't exist yet
+os.makedirs(CHECKPOINT_PATH, exist_ok=True)
+
+# For each file, check whether it already exists. If not, try downloading it.
+for file_name in pretrained_files:
+    file_path = os.path.join(CHECKPOINT_PATH, file_name)
+    if not os.path.isfile(file_path):
+        file_url = base_url + file_name
+        print(f"Downloading {file_url}...")
+        try:
+            urllib.request.urlretrieve(file_url, file_path)
+        except HTTPError as e:
+            print("Something went wrong. Please try to download the file from the GDrive folder, or contact the author with the full output including the following error:\n", e)
+
+# Convert images from 0-1 to 0-255 (integers)
+def discretize(sample):
+    return (sample * 255).to(torch.int32)
+
+# Transformations applied on each image => make them a tensor and discretize
+transform = transforms.Compose([transforms.ToTensor(),
+                                discretize])
+
+# Loading the training dataset. We need to split it into a training and validation part
+train_dataset = MNIST(root=DATASET_PATH, train=True, transform=transform, download=True)
+pl.seed_everything(42)
+train_set, val_set = torch.utils.data.random_split(train_dataset, [50000, 10000])
+
+# Loading the test set
+test_set = MNIST(root=DATASET_PATH, train=False, transform=transform, download=True)
+
+# We define a set of data loaders that we can use for various purposes later.
+# Note that for actually training a model, we will use different data loaders
+# with a lower batch size.
+train_loader = data.DataLoader(train_set, batch_size=256, shuffle=False, drop_last=False)
+val_loader = data.DataLoader(val_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4)
+test_loader = data.DataLoader(test_set, batch_size=64, shuffle=False, drop_last=False, num_workers=4)
+
+def show_imgs(imgs, title=None, row_size=4):
+    # Form a grid of pictures (we use max. 8 columns)
+    num_imgs = imgs.shape[0] if isinstance(imgs, torch.Tensor) else len(imgs)
+    is_int = imgs.dtype==torch.int32 if isinstance(imgs, torch.Tensor) else imgs[0].dtype==torch.int32
+    nrow = min(num_imgs, row_size)
+    ncol = int(math.ceil(num_imgs/nrow))
+    imgs = torchvision.utils.make_grid(imgs, nrow=nrow, pad_value=128 if is_int else 0.5)
+    np_imgs = imgs.cpu().numpy()
+    # Plot the grid
+    plt.figure(figsize=(1.5*nrow, 1.5*ncol))
+    plt.imshow(np.transpose(np_imgs, (1,2,0)), interpolation='nearest')
+    plt.axis('off')
+    if title is not None:
+        plt.title(title)
+    plt.show()
+    plt.close()
+
+show_imgs([train_set[i][0] for i in range(8)])
+
+class ImageFlow(pl.LightningModule):
+
+    def __init__(self, flows, import_samples=8):
+        """
+        Inputs:
+            flows - A list of flows (each a nn.Module) that should be applied on the images.
+            import_samples - Number of importance samples to use during testing (see explanation below). Can be changed at any time
+        """
+        super().__init__()
+        self.flows = nn.ModuleList(flows)
+        self.import_samples = import_samples
+        # Create prior distribution for final latent space
+        self.prior = torch.distributions.normal.Normal(loc=0.0, scale=1.0)
+        # Example input for visualizing the graph
+        self.example_input_array = train_set[0][0].unsqueeze(dim=0)
+
+    def forward(self, imgs):
+        # The forward function is only used for visualizing the graph
+        return self._get_likelihood(imgs)
+
+    def encode(self, imgs):
+        # Given a batch of images, return the latent representation z and ldj of the transformations
+        z, ldj = imgs, torch.zeros(imgs.shape[0], device=self.device)
+        for flow in self.flows:
+            z, ldj = flow(z, ldj, reverse=False)
+        return z, ldj
+
+    def _get_likelihood(self, imgs, return_ll=False):
+        """
+        Given a batch of images, return the likelihood of those.
+        If return_ll is True, this function returns the log likelihood of the input.
+        Otherwise, the ouptut metric is bits per dimension (scaled negative log likelihood)
+        """
+        z, ldj = self.encode(imgs)
+        log_pz = self.prior.log_prob(z).sum(dim=[1,2,3])
+        log_px = ldj + log_pz
+        nll = -log_px
+        # Calculating bits per dimension
+        bpd = nll * np.log2(np.exp(1)) / np.prod(imgs.shape[1:])
+        return bpd.mean() if not return_ll else log_px
+
+    @torch.no_grad()
+    def sample(self, img_shape, z_init=None):
+        """
+        Sample a batch of images from the flow.
+        """
+        # Sample latent representation from prior
+        if z_init is None:
+            z = self.prior.sample(sample_shape=img_shape).to(device)
+        else:
+            z = z_init.to(device)
+
+        # Transform z to x by inverting the flows
+        ldj = torch.zeros(img_shape[0], device=device)
+        for flow in reversed(self.flows):
+            z, ldj = flow(z, ldj, reverse=True)
+        return z
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        # An scheduler is optional, but can help in flows to get the last bpd improvement
+        scheduler = optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.99)
+        return [optimizer], [scheduler]
+
+    def training_step(self, batch, batch_idx):
+        # Normalizing flows are trained by maximum likelihood => return bpd
+        loss = self._get_likelihood(batch[0])
+        self.log('train_bpd', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._get_likelihood(batch[0])
+        self.log('val_bpd', loss)
+
+    def test_step(self, batch, batch_idx):
+        # Perform importance sampling during testing => estimate likelihood M times for each image
+        samples = []
+        for _ in range(self.import_samples):
+            img_ll = self._get_likelihood(batch[0], return_ll=True)
+            samples.append(img_ll)
+        img_ll = torch.stack(samples, dim=-1)
+
+        # To average the probabilities, we need to go from log-space to exp, and back to log.
+        # Logsumexp provides us a stable implementation for this
+        img_ll = torch.logsumexp(img_ll, dim=-1) - np.log(self.import_samples)
+
+        # Calculate final bpd
+        bpd = -img_ll * np.log2(np.exp(1)) / np.prod(batch[0].shape[1:])
+        bpd = bpd.mean()
+
+        self.log('test_bpd', bpd)
+
+class Dequantization(nn.Module):
+
+    def __init__(self, alpha=1e-5, quants=256):
+        """
+        Inputs:
+            alpha - small constant that is used to scale the original input.
+                    Prevents dealing with values very close to 0 and 1 when inverting the sigmoid
+            quants - Number of possible discrete values (usually 256 for 8-bit image)
+        """
+        super().__init__()
+        self.alpha = alpha
+        self.quants = quants
+
+    def forward(self, z, ldj, reverse=False):
+        if not reverse:
+            z, ldj = self.dequant(z, ldj)
+            z, ldj = self.sigmoid(z, ldj, reverse=True)
+        else:
+            z, ldj = self.sigmoid(z, ldj, reverse=False)
+            z = z * self.quants
+            ldj += np.log(self.quants) * np.prod(z.shape[1:])
+            z = torch.floor(z).clamp(min=0, max=self.quants-1).to(torch.int32)
+        return z, ldj
+
+    def sigmoid(self, z, ldj, reverse=False):
+        # Applies an invertible sigmoid transformation
+        if not reverse:
+            ldj += (-z-2*F.softplus(-z)).sum(dim=[1,2,3])
+            z = torch.sigmoid(z)
+            # Reversing scaling for numerical stability
+            ldj -= np.log(1 - self.alpha) * np.prod(z.shape[1:])
+            z = (z - 0.5 * self.alpha) / (1 - self.alpha)
+        else:
+            z = z * (1 - self.alpha) + 0.5 * self.alpha  # Scale to prevent boundaries 0 and 1
+            ldj += np.log(1 - self.alpha) * np.prod(z.shape[1:])
+            ldj += (-torch.log(z) - torch.log(1-z)).sum(dim=[1,2,3])
+            z = torch.log(z) - torch.log(1-z)
+        return z, ldj
+
+    def dequant(self, z, ldj):
+        # Transform discrete values to continuous volumes
+        z = z.to(torch.float32)
+        z = z + torch.rand_like(z).detach()
+        z = z / self.quants
+        ldj -= np.log(self.quants) * np.prod(z.shape[1:])
+        return z, ldj
+
+
+
+## Testing invertibility of dequantization layer
+pl.seed_everything(42)
+orig_img = train_set[0][0].unsqueeze(dim=0)
+ldj = torch.zeros(1,)
+dequant_module = Dequantization()
+deq_img, ldj = dequant_module(orig_img, ldj, reverse=False)
+reconst_img, ldj = dequant_module(deq_img, ldj, reverse=True)
+
+d1, d2 = torch.where(orig_img.squeeze() != reconst_img.squeeze())
+if len(d1) != 0:
+    print("Dequantization was not invertible.")
+    for i in range(d1.shape[0]):
+        print("Original value:", orig_img[0,0,d1[i], d2[i]].item())
+        print("Reconstructed value:", reconst_img[0,0,d1[i], d2[i]].item())
+else:
+    print("Successfully inverted dequantization")
+
+def visualize_dequantication(quants, prior=None):
+    """
+    Function for visualizing the dequantization values of discrete values in continous space
+    """
+    # Prior over discrete values. If not give, a uniform is assumed
+    if prior is None:
+        prior = np.ones(quants, dtype=np.float32) / quants
+    prior = prior / prior.sum() # Ensure proper categorical distribution
+
+    inp = torch.arange(-4, 4 ,0.01).view(-1, 1, 1, 1) # Possible continuous values we want to consider
+    ldj = torch.zeros(inp.shape[0])
+    dequant_module = Dequantization(quants=quants)
+    # Invert dequantization on continous values to find corresponding discrete value
+    out, ldj = dequant_module.forward(inp, ldj, reverse=True)
+    inp, out, prob = inp.squeeze().numpy(), out.squeeze().numpy(), ldj.exp().numpy()
+    prob = prob * prior[out] # Probability scaled by categorical prior
+
+    # Plot volumes and continous distribution
+    sns.set_style("white")
+    fig = plt.figure(figsize=(6, 3))
+    x_ticks = []
+    for v in np.unique(out):
+        indices = np.where(out == v)
+        color = to_rgb(f"C{v}")
+        plt.fill_between(inp[indices], prob[indices], np.zeros(indices[0].shape[0]), color=color+(0.5,), label=str(v))
+        plt.plot([inp[indices[0][0]]]*2, [0, prob[indices[0][0]]], color=color)
+        plt.plot([inp[indices[0][-1]]]*2, [0, prob[indices[0][-1]]], color=color)
+        x_ticks.append(inp[indices[0][0]])
+    x_ticks.append(inp.max())
+    plt.xticks(x_ticks, [f"{x:.1f}" for x in x_ticks])
+    plt.plot(inp, prob, color=(0.0, 0.0, 0.0))
+    # Set final plot properties
+    plt.ylim(0, prob.max()*1.1)
+    plt.xlim(inp.min(), inp.max())
+    plt.xlabel("z")
+    plt.ylabel("Probability")
+    plt.title(f"Dequantication distribution for {quants} discrete values")
+    plt.legend()
+    plt.show()
+    plt.close()
+
+visualize_dequantication(quants=8)
+
+class VariationalDequantization(Dequantization):
+
+    def __init__(self, var_flows, alpha=1e-5):
+        """
+        Inputs:
+            var_flows - A list of flow transformations to use for modeling q(u|x)
+            alpha - Small constant, see Dequantization for details
+        """
+        super().__init__(alpha=alpha)
+        self.flows = nn.ModuleList(var_flows)
+
+    def dequant(self, z, ldj):
+        z = z.to(torch.float32)
+        img = (z / 255.0) * 2 - 1 # We condition the flows on x, i.e. the original image
+
+        # Prior of u is a uniform distribution as before
+        # As most flow transformations are defined on [-infinity,+infinity], we apply an inverse sigmoid first.
+        deq_noise = torch.rand_like(z).detach()
+        deq_noise, ldj = self.sigmoid(deq_noise, ldj, reverse=True)
+        for flow in self.flows:
+            deq_noise, ldj = flow(deq_noise, ldj, reverse=False, orig_img=img)
+        deq_noise, ldj = self.sigmoid(deq_noise, ldj, reverse=False)
+
+        # After the flows, apply u as in standard dequantization
+        z = (z + deq_noise) / 256.0
+        ldj -= np.log(256.0) * np.prod(z.shape[1:])
+        return z, ldj
